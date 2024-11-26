68d54316dfc1daa9bf122b90f09c1053c109ab33
jnabin <jahangirnabin2@gmail.com>
2024-11-26 23:01:46 +0600

    User domain with repository and domain events with mediatr

--- Bookify.Domain/Abstraction/Entity.cs
+++ Bookify.Domain/Abstraction/Entity.cs
@@ -8,7 +8,21 @@ namespace Bookify.Domain.Abstraction
 {
     public abstract class Entity
     {
+        private readonly List<IDomainEvent> _domainEvents = new();
         protected Entity(Guid id) => Id = id;
         public Guid Id { get; init; }
+
+        public IReadOnlyList<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents;
+        }
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+        protected void RaiseDomainEvents(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
     }
 }
---
+++ Bookify.Domain/Users/Events/UserCreatedDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Domain.Abstraction;
+
+namespace Bookify.Domain.Users.Events
+{
+    public sealed record UserCreatedDomainEvent(Guid UserId):IDomainEvent; 
+}
