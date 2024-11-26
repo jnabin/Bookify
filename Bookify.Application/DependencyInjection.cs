25f11cba3dcea9e0238fbee8823b5a52f0700f0b
jnabin <jahangirnabin2@gmail.com>
2024-11-27 02:46:13 +0600

    adding application layer with dependency injection

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+            return services;
+        }
+    }
+}
